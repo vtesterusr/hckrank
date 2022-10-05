bb9bb27d3d6a583029eb2ba1329a1f9b84a8d09a
vtesterusr <115096276+vtesterusr@users.noreply.github.com>
2022-10-05 23:24:25 +0800

    Update ScannerSInSout.java

--- ScannerSInSout.java
+++ ScannerSInSout.java
@@ -4,33 +4,21 @@ public class ScannerSInSout {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        // Write your code here.
+        scannerInOut(scan);
+    }
+    
+    public static void scannerInOut (Scanner scan){
+        
         int i = scan.nextInt();
         double d = scan.nextDouble();
         String s =scan.next();
-        //System.out.println("Value of:-"+s);
-        //int len = scan.next().length();
         
          while(scan.hasNext()){
                 //System.out.println("Value of:-"+s);
                 s=s+" "+scan.next();
                 //System.out.println("Value of:-"+s);
             }
-
-        
-        // Write your code here.
-        
-                    
-            
-            
-      /*  for(int j=-1; j < len ;j++){
-            while(scan.hasNext()){
-                s=s+" "+scan.next();
-                
-            }
-            System.out.println("Value of"+s);
-            
-            System.out.println("Value of"+s);
-        } */
         
         if(s != null){
            System.out.println("String: "+s); 
